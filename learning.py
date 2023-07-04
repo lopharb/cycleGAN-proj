3b81df6e619ddb90f82bb6831e91d8a5de6c9597
lopharb <vanekshkel@gmail.com>
2023-07-05 02:42:45 +0300

    architectures finished

--- learning.py
+++ learning.py
@@ -13,6 +13,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from PIL import Image
+import custom
 
 # transforms
 
--- model.py
+++ model.py
@@ -30,33 +30,107 @@ class ResidualBlock(nn.Module):
         return self.block(x) + x
 
 
+class Custom(nn.Module):
+    def __init__(self,  *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    def forward(self, x):
+        print(x.shape)
+        return x
+
+
 class Generator(nn.Module):
-    def __init__(self, n_blocks) -> None:
+    def __init__(self, n_blocks=9) -> None:
         super(Generator, self).__init__()
-        in_channels = 3  # since we have an rgb image
-        res_channels = 64
+
+        initial_channels = in_channels = 3  # since we have an rgb image
+        res_channels = 32
 
         # initial convolution to get to a desired amount of channels for residual blocks
-        model = [
-            nn.RefectionPad2d(2),
+        net = [
+            nn.ReflectionPad2d(2),
             nn.Conv2d(in_channels, res_channels, 5),
             nn.InstanceNorm2d(res_channels),
-            nn.ReLU()
+            nn.ReLU(inplace=True)
         ]
 
-        # add downsampling
+        # downsampling (by factor of 4)
+        in_channels = res_channels
+        for _ in range(2):
+            res_channels *= 2
+            net += [
+                nn.Conv2d(in_channels, res_channels, 3, stride=2, padding=1),
+                nn.InstanceNorm2d(res_channels),
+                nn.ReLU(inplace=True),
+            ]
+            in_channels = res_channels
 
         # adding residual blocks
         for _ in range(n_blocks):
-            model += [ResidualBlock(res_channels)]
+            net += [ResidualBlock(res_channels)]
 
-        # add upsampling
+        # upsampling (also by factor of 4)
+        for _ in range(2):
+            res_channels //= 2
+            net += [
+                nn.Upsample(scale_factor=2),
+                nn.Conv2d(in_channels, res_channels, 3, padding=1),
+                nn.ReLU(inplace=True)
+            ]
+            in_channels = res_channels
 
-        # adding final layer
-        model += [
-            nn.RefectionPad2d(2),
-            nn.Conv2d(res_channels, in_channels, 5),
+        # final layer
+        net += [
+            nn.ReflectionPad2d(2),
+            nn.Conv2d(res_channels, initial_channels, 5),
             nn.Tanh()
         ]
 
-        self.model = nn.Sequential(*model)
+        self.net = nn.Sequential(*net)
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class DiscBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, normalize=True) -> None:
+        super(DiscBlock, self).__init__()
+        block = [nn.Conv2d(in_channels, out_channels, 4, stride=2, padding=1)]
+        if normalize:
+            block.append(nn.InstanceNorm2d(out_channels))
+        block.append(nn.ReLU(inplace=True))
+
+        self.block = nn.Sequential(*block)
+
+    def forward(self, x):
+        return self.block(x)
+
+
+class Discriminator(nn.Module):
+    def __init__(self) -> None:
+        super(Discriminator, self).__init__()
+
+        in_channels = 3  # still taking rgb image as input
+
+        self.net = nn.Sequential(
+            DiscBlock(in_channels, 32, normalize=False),
+            Custom(),  # 32 128 128
+            DiscBlock(32, 64),
+            Custom(),  # 64 64 64
+            DiscBlock(64, 128),
+            Custom(),  # 128 32 32
+            DiscBlock(128, 256),
+            Custom(),  # 256 16 16
+            DiscBlock(256, 512),
+            Custom(),  # 512 8 8
+            nn.Conv2d(512, 1, 8),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+dis = Discriminator()
+x = torch.randn((1, 3, 256, 256))
+print(dis(x).item())
